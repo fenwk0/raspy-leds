e5732e6dc237bd5c85a702f67cdd052e232b7baf
fenwk0 <karl.fenwick@gmail.com>
2025-01-13 05:47:34 +0000

    super simple led test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Simple Raspio LED project
+
+Based on https://rasp.io/wp-content/uploads/2016/04/Pro-Hat-GPIO-Zero.pdf
+
---
+++ led-001.py
@@ -0,0 +1,7 @@
+from gpiozero import LED
+green = LED(5)
+green.on()
+# The argument to the function may be any descriptive text
+input("Press the Enter key to continue: ")
+
+
